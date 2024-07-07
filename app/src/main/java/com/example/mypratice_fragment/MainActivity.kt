a7e2502d86664270df3387539b0e500f5b8dbc3f
PudCheetah <wer212131@gmail.com>
2024-07-07 17:16:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mypratice_fragment/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.mypratice_fragment
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mypratice_fragment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btn1.setOnClickListener {
+            val fragmentTransation = supportFragmentManager.beginTransaction()
+            fragmentTransation.add(binding.fragmentContainerView.id, MyFragment.instance)
+            fragmentTransation.commit()
+        }
+    }
+}
